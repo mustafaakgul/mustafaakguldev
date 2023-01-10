7a6a99be6a0b0941b7e33f6bc72d56172a3ee57f
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2023-01-11 00:57:01 +0300

    add refactoring

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=100)
+    subject = models.CharField(max_length=30)
+    message = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.name
